c2a246ca2bded69366fc6b18ccd5f52bb0aa0ee4
Stephanie Glover <stephanie.n.glover@disney.com>
2018-05-25 14:52:03 -0700

    added experiences table

---
+++ db/seeds/01_adds_experiences_data.sql
@@ -0,0 +1,9 @@
+\c serenity
+
+INSERT INTO experiences(experience_name, description)
+VALUES ('Classes', 'Serenity offers a range of classes that will allow you to restore body, mind, and spirit.'),
+('Facials', 'Our spa facials are great for skin restoration and maintenance. Our experienced estheticians use products with only natural ingredients.'),
+('Massages', 'Serenity massages are provided by highly-trained therapists who can give customized experiences based on your specific needs.'),
+('Spa Amenities', 'Our facility offers common areas in which our guests can choose from a range of serene amenities.');
+
+SELECT * FROM experiences;
