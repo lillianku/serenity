02d22421d274174ffbf7916b7514fc59074185f5
Lilli Ku <lilli.c.ku@disney.com>
2018-05-25 11:57:07 -0700

    creates serenity db and experiences, services, and guests tables

--- db/migrations/01_create_db.sql
+++ db/migrations/01_create_db.sql
@@ -1,1 +1,31 @@
+CREATE DATABASE serenity;
 
+\c serenity
+
+CREATE TABLE experiences(
+  experience_id SERIAL PRIMARY KEY,
+  experience_name VARCHAR(50) NOT NULL,
+  description VARCHAR(500) NOT NULL
+);
+
+CREATE TABLE services(
+  service_id SERIAL PRIMARY KEY,
+  experience_id INTEGER REFERENCES experiences(experience_id),
+  service_name VARCHAR(50) NOT NULL,
+  description VARCHAR(1000) NOT NULL,
+  allergens VARCHAR(50),
+  duration INTEGER NOT NULL,
+  price INTEGER NOT NULL
+);
+
+CREATE TABLE guests(
+  guest_id SERIAL PRIMARY KEY,
+  username VARCHAR(50) NOT NULL,
+  first_name VARCHAR(50) NOT NULL,
+  last_name VARCHAR(50) NOT NULL,
+  email VARCHAR(50) NOT NULL,
+  phone VARCHAR(50) NOT NULL,
+  allergies VARCHAR(250),
+  comments VARCHAR(500),
+  created_on TIMESTAMP NOT NULL DEFAULT NOW()
+);
