4f40072a6be7ac9273fe3a5b58e281d82d8ce79d
Lilli Ku <lilli.c.ku@disney.com>
2018-05-29 11:31:15 -0700

    pulled changes

--- client/src/components/GuestsAll.js
+++ client/src/components/GuestsAll.js
@@ -2,3 +2,41 @@
 //Should list each guest as a clickable Link
 //Link should link to individual Guest that was clicked on
 //There should be a button that will Link to Add New Guest page
+
+import React, { Component } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+
+class GuestsAll extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {guests: null};
+  }
+
+  render() {
+    const { guests } = this.state;
+    if(!guests) { return <div>Guests are Loading</div>; }
+
+    return (
+      <div className="GuestsAll">
+        {guests.map(guest => this.renderGuestLink(guest))}
+      </div>
+    );
+  }
+
+  renderGuestLink(guest) {
+    return (
+      <li key={guest.guest_id}>
+        <Link to={`/guests/${guest.guest_id}`}>{guest.first_name} {guest.last_name}</Link>
+      </li>
+    );
+  }
+
+  componentDidMount() {
+    axios.get('/guests').then(res=> {
+      this.setState({guests: res.data})
+    });
+  }
+}
+
+export default GuestsAll;
