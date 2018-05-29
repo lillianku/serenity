1e6a89ea3d6ef610134656398e46a29e739e24e0
Dani Espinoza <Danielle.V.Espinoza@disney.com>
2018-05-29 09:28:44 -0700

    add routes and model for experiences

--- models/experiences.js
+++ models/experiences.js
@@ -1,2 +1,24 @@
 //This is where we define the functions for the ExperiencesRouter
 //find, findOne, fineOneAndUpdate, delete, save, etc.
+const db = require('.././db/config'),
+      Experiences = {};
+
+//find all experiences
+Experiences.find = () => {
+  return db.query(`SELECT * FROM experiences`)
+};
+
+//find experiences by experienceid
+Experiences.findById = (experienceid) => {
+  const {id} = experienceid;
+  return db.one(`SELECT * FROM experiences
+    WHERE experience_id=$1`, id)
+};
+
+//find experienceid then service by id
+Experiences.findByTwoIds = (serviceid) => {
+  const {id} = serviceid;
+  return db.one(`SELECT * FROM services
+    WHERE experience_id=$1 AND service_id =$2`, id)
+}
+//for testing 
--- routes/experiencesrouter.js
+++ routes/experiencesrouter.js
@@ -1,2 +1,30 @@
 //This is where we will use the methods from methods/experiences.js
 //This is where we do the .then/.catch stuff
+const express = require('express'),
+      ExperiencesRouter = express.Router(),
+      db = require('.././db/donfig'),
+      Experiences = require('.././models/experiences');
+
+ExperiencesRouter.get('/', (req,res) => {
+  Experiences.find().then((experiences) => {
+    res.json(experiences);
+  }).catch(error => {
+    res.status(500).json({message: 'An error occurred'});
+  });
+});
+
+ExperiencesRouter.get('/:experienceid', (req,res) => {
+  Experiences.findById(req.params).then((experience) => {
+    res.json(experience);
+  }).catch(error => {
+    res.status(500).json({message: 'An error occurred'});
+  });
+});
+
+ExperiencesRouter.get('/:experienceid/:serviceid', (req,res) => {
+  Experiences.findByTwoIds(req.params).then((service) => {
+    res.json(service);
+  }).catch(error => {
+    res.status(500).json({message: 'An error occurred'});
+  });
+});
