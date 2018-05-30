a14b45ad065fbd7744d34d745888e14411fe9a01
pforl001 <lindsay.m.pforsich@disney.com>
2018-05-30 10:33:37 -0700

    start to add code for service component

--- client/src/components/Service.js
+++ client/src/components/Service.js
@@ -1,2 +1,34 @@
 //Create component for each Service
 //Details for this specific Service should be rendered
+import React, { Component } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+
+class Service extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {service: null};
+  }
+
+  render() {
+    const { service } = this.state;
+    if(!service) { return <div>Loading Service Description</div>; }
+
+    return (
+      <div className="Service">
+        <li key={service.service_id}>
+          <Link to={`/experiences/${service.service_id}`}>{service.service_name}</Link>
+        </li>
+      </div>
+    );
+  }
+
+  componentDidMount() {
+    const {id} = this.props.match.params;
+    axios.get(`/experiences/${id}`).then(res => {
+      this.setState({service: res.data})
+    });
+  }
+}
+
+export default Service;
