7a50c34efd6e3c7beca23a23d91f55d32c2e454b
pforl001 <lindsay.m.pforsich@disney.com>
2018-05-31 10:16:20 -0700

    post to packages database on submit button click

--- client/src/components/Service.js
+++ client/src/components/Service.js
@@ -6,13 +6,14 @@ import axios from 'axios';
 class Service extends Component {
   constructor(props) {
     super(props)
-    this.state = {service: null};
+    this.state = {service: null, guest_id: ''};
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
 
   render() {
-    const { service } = this.state;
+    const { service, guest_id } = this.state;
     if(!service) { return <div>Loading Service</div>; }
-    console.log(service.description);
     return (
       <div className="Service">
         <h2>{service.service_name}</h2>
@@ -20,6 +21,12 @@ class Service extends Component {
         <h3><b>Potential Allergens:</b> {this.renderAllergens()}</h3>
         <h3><b>Service Duration:</b> {service.duration} minutes</h3>
         <h3><b>Price:</b> ${service.price}</h3>
+
+        <form onSubmit={this.handleSubmit}>
+         <input type="text" value={guest_id} name="guest_id" placeholder="guest id" onChange={this.handleChange}/>
+         <input type="submit" value="Add to your Package" />
+       </form>
+
       </div>
     );
   }
@@ -32,6 +39,27 @@ class Service extends Component {
     }
     return (`${service.allergens}`);
   }
+
+  handleChange (event) {
+    const {value, name} = event.target;
+    this.setState({
+      [name]: value
+    });
+   }
+
+   handleSubmit(event) {
+     const {service}= this.state;
+     let guest_id = parseInt(this.state.guest_id, 10);
+     console.log(guest_id);
+     const service_id = service.service_id;
+     console.log(service_id);
+     event.preventDefault();
+     axios.post(`/guests/${guest_id}/package`, {guest_id, service_id}).then(res => {
+       this.props.history.push(`/guests/${guest_id}/package`); // change this to services page & add an alert
+     }).catch(event => {
+       alert('Service could not be added to package');
+     });
+   }
 
   componentDidMount() {
     const {experienceid, serviceid} = this.props.match.params;
